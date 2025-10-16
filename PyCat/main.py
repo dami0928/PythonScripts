a7f984fa928e36626c741b18af81d14545c88445
dami <damiano.gubiani@gmail.com>
2025-10-16 08:49:27 +0200

    update
    update

--- PyCat/main.py
+++ PyCat/main.py
@@ -1,56 +1,131 @@
-import socket
-import argparse
+import argparse , socket , threading , subprocess , shlex , textwrap , sys , time , signal
 
-def server_tcp(target,port):
 
-    server_bind = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server_bind.bind((target,port))
-    server_bind.listen()
-    print(f'listening on {target} {port}')
-    client_sock,addr = server_bind.accept()
-    
-    while client_sock:
+
+class NetCat:
+
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+   
+    def signal_handler(self, signum):
+        print('\n[*] User requested an interrupt. Exiting gracefully.')
+        time.sleep(0.5)
+        sys.exit(0)
+
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
         try:
-            
-            print(f'recived connection from {addr[0]} {addr[1]}')
-            print(client_sock.recv(2048).decode())
-
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                    if response:
+                        print(response)
+                        buffer = input('')
+                        buffer += '\n'
+                        self.socket.send(buffer.encode())
         except KeyboardInterrupt:
-            exit(0)
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
 
-def client_sender(target,port):
+    def listen(self):
+        
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        print(f'listening on {self.args.target} {self.args.port}')
 
-    client_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client_sock.connect((target,port))
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target=self.handle, args=(client_socket,)
+            )
+            client_thread.start()
+        
 
-    while 1:
-        try:
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
 
-            resp = ""
-            resp_len = 1
-
-            while resp_len:
-
-                data = client_sock.recv(4096)
-                resp_len = len(data)
-                resp += data
-
-                if resp_len > 4096:
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
                     break
-
-                print(resp.decode())
-                
-                buffer = input("").encode()
-                buffer += "\n"
-
-                client_sock.send(buffer)
-
-        except:
-            exit(0)
+                with open(self.args.upload, 'wb') as f:
+                    f.write(file_buffer)
+                message = f'Saved file {self.args.upload}'
+                client_socket.send(message.encode())
+        elif self.args.command:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b'CMD: #> ')
+                    while '\n' not in cmd_buffer.decode: 
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close
+                    sys.exit()  
+    
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
 
 
 
 
 if __name__ == '__main__':
-    client_sender('google.com',443)
+    parser = argparse.ArgumentParser(description='BHP Net Tool', formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=textwrap.dedent('''Example:\nnetcat.py -t 192.168.1.108 - p 555 -l -c #command shell
+                                     netcat.py -t 192.168.1.108 - p 555 -l -u=mytest.txt #file to upload
+                                     netcat.py -t 192.168.1.108 - p 555 -l -e \"cat /etc/passwd\" #execute command
+                                     echo ABC | ./netcat.py -t 192.168.1.108 - p 135 #echo text to port 135
+                                     netcat.py -t 192.168.1.108 - p 555 #connect to server'''))
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specific command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='0.0.0.0',help="specific IP")
+    parser.add_argument('-u', '--upload', help='upload file')
+    args = parser.parse_args()
+
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
     
+    nc = NetCat(args, buffer.encode())
+    nc.run()
+
+    def execute(cmd):
+        cmd = cmd.strip()
+        if not cmd:
+            return
+        output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+
+        return output.decode()
